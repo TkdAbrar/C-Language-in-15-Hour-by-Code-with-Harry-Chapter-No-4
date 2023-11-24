9c177e18300c8505e4c3787ab48ae1983c5708cd
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2023-11-24 08:28:34 -0800

    C Language in 15 Hour by Code with Harry Chapter No 4
    Chapter No 4

--- 04_06_quick_quiz.c
+++ 04_06_quick_quiz.c
@@ -4,13 +4,13 @@ int main()
 {
     int i = 0;
     int n;
-    printf("Enter the value of n \n");
+    printf("Enter the value of n\n");
     scanf("%d", &n);
     do
     {
         printf("The number is %d\n", i + 1);
         i++;
-    } while (i < n);
+    } while (i <= n);
 
     return 0;
 }
